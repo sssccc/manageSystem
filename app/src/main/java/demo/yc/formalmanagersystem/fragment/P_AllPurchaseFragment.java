e4dbcbb6a186f19d91bcc475affffa160e8f6761
caoweizhao <13560321319@163.com>
2016-10-24 19:53:43 +0800

    AllPurchaseFragment

--- app/src/main/java/demo/yc/formalmanagersystem/fragment/P_AllPurchaseFragment.java
+++ app/src/main/java/demo/yc/formalmanagersystem/fragment/P_AllPurchaseFragment.java
@@ -1,6 +1,5 @@
 package demo.yc.formalmanagersystem.fragment;
 
-import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Color;
@@ -22,7 +21,6 @@ import android.widget.ListView;
 import android.widget.PopupWindow;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.android.volley.VolleyError;
 
@@ -39,16 +37,41 @@ import demo.yc.formalmanagersystem.adapter.MyAdapterForPurchase;
 import demo.yc.formalmanagersystem.database.MyDBHandler;
 import demo.yc.formalmanagersystem.models.Purchase;
 import demo.yc.formalmanagersystem.util.JsonUtil;
+import demo.yc.formalmanagersystem.util.ToastUtil;
 import demo.yc.formalmanagersystem.util.VolleyUtil;
 import demo.yc.formalmanagersystem.view.RefreshableView;
 
 /**
  * Created by Administrator on 2016/7/26.
  */
- public class P_AllPurchaseFragment extends Fragment implements View.OnClickListener {
+public class P_AllPurchaseFragment extends Fragment implements View.OnClickListener {
 
-    private ImageView top_layout_menu;
-    private boolean once = true;
+    private ImageView top_layout_menu;  //切换菜单按钮
+    private boolean once = true;    //用于标志本地无数据时，是否第一次进行网络加载
+    private RelativeLayout direction;    //弹出选项框按钮
+    private ImageView directionImg;     //弹出框指示图标
+    private boolean down;   //标志当前是否弹出选项框
+    private List<Purchase> purchases = new ArrayList<>();//全部采购
+
+    //用于对purchases进行筛选
+    private List<Purchase> temp2 = new ArrayList<>();
+    private ListView listView;
+    private MyAdapterForPurchase allPurchaseAdapter;
+
+    //用于分类显示
+    private PopupWindow popupwindow;
+    private TextView allPurchase;
+    private TextView pass;
+    private TextView refuse;
+    private View customView;
+    private TextView toBeHandle;
+
+    private LinearLayout backToTop;     //返回顶部
+    private VolleyUtil volleyUtil = new VolleyUtil();
+    private ExecutorService executor;
+    private RefreshableView refreshableView;    //下拉刷新
+
+    //根据数据更新的情况更新UI
     private Handler handler = new Handler() {
         @Override
         public void handleMessage(Message msg) {
@@ -59,20 +82,20 @@ import demo.yc.formalmanagersystem.view.RefreshableView;
             if (msg.what == 1) {
                 Log.d("myTag", "failure");
                 if (getActivity() != null) {
-                    Toast.makeText(getActivity(), "更新失败，请重试！", Toast.LENGTH_SHORT).show();
+                    ToastUtil.createToast(getActivity(), "更新失败，请重试！");
                 }
             }
             //更新成功时(我的采购)
             else {
                 purchases = (List<Purchase>) msg.obj;
+                //本地数据库中存在数据
                 if (purchases.size() != 0) {
-                    Log.d("myTag", "success");
                     for (Purchase purchase : purchases
                             ) {
                         temp2.add(purchase);
                     }
                     if (temp2.size() != 0) {
-                        if (getActivity() != null) {    //快速切换时候，新开的线程中Fragment的Activity还没Created
+                        if (getActivity() != null) {
                             allPurchaseAdapter = new MyAdapterForPurchase(getActivity(), R.layout.item, temp2);
                         }
                         if (allPurchaseAdapter != null) {
@@ -80,12 +103,10 @@ import demo.yc.formalmanagersystem.view.RefreshableView;
                             allPurchaseAdapter.notifyDataSetChanged();
                         }
                     }
-                    if (flag) {
-                        flag = false;
-                        Toast.makeText(getActivity(), "更新成功！", Toast.LENGTH_SHORT).show();
-                    }
                     doAfterAsyTask();
-                } else {
+                }
+                //本地无数据，自动进行一次网络加载
+                else {
                     if (once) {
                         once = false;
                         refreshData();
@@ -95,55 +116,12 @@ import demo.yc.formalmanagersystem.view.RefreshableView;
 
         }
     };
-    private RelativeLayout direction;
-    private ImageView directionImg;
-    private boolean down;
-    private boolean flag;
-
-    //全部采购
-    private List<Purchase> purchases = new ArrayList<>();
-
-    private ListView listView;
-    private MyAdapterForPurchase allPurchaseAdapter;
-
-    float dY = 0;
-    float uY = 0;
-
-    private List<Purchase> temp1 = new ArrayList<>();
-    private List<Purchase> temp2 = new ArrayList<>();
-
-    //用于分类
-    private PopupWindow popupwindow;
-    private TextView allPurchase;
-    private TextView pass;
-    private TextView refuse;
-    private View customView;
-    private TextView toBeHandle;
-
-    private LinearLayout backToTop;
-    private VolleyUtil volleyUtil = new VolleyUtil();
-    private ExecutorService executor;
-
-    private RefreshableView refreshableView;
-
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-
-    }
 
     @Override
     public void onPause() {
         super.onPause();
-        Log.d("tag","onPause");
         refreshableView.finishRefreshing("pause");
         P_PropertyManagementFragment.isInitial = true;
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        //P_PropertyManagementFragment.isInitial = true;
     }
 
     @Nullable
@@ -159,17 +137,19 @@ import demo.yc.formalmanagersystem.view.RefreshableView;
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         if (getActivity() != null) {
+            //设置下拉刷新监听
             refreshableView.setOnRefreshListener(new RefreshableView.PullToRefreshListener() {
                 @Override
                 public void onRefresh() {
                     refreshData();
                 }
             }, 1);
-            top_layout_menu = (ImageView)getActivity().findViewById(R.id.top_layout_menu);
+            top_layout_menu = (ImageView) getActivity().findViewById(R.id.top_layout_menu);
             direction = (RelativeLayout) getActivity().findViewById(R.id.direction_in_top);
             direction.setOnClickListener(this);
             directionImg = (ImageView) getActivity().findViewById(R.id.direction_img);
             executor = Executors.newFixedThreadPool(2);
+            //从本地数据库读取数据
             readDataFromSQLite();
         }
     }
@@ -200,8 +180,8 @@ import demo.yc.formalmanagersystem.view.RefreshableView;
         }
     }
 
+    //注册事件
     private void initEvents() {
-        //注册事件
         allPurchase.setOnClickListener(this);
         pass.setOnClickListener(this);
         refuse.setOnClickListener(this);
@@ -210,7 +190,7 @@ import demo.yc.formalmanagersystem.view.RefreshableView;
         top_layout_menu.setOnClickListener(this);
     }
 
-    //获取listview拖动高度
+    //获取ListView拖动高度
     public int getScrollY() {
         View c = listView.getChildAt(0);
         if (c == null) {
@@ -222,25 +202,26 @@ import demo.yc.formalmanagersystem.view.RefreshableView;
     }
 
     //重置资产状态分类的样式
-    public  void resetStatusStyle() {
-        if(this.isAdded())
-        {
+    public void resetStatusStyle() {
+        if (this.isAdded()) {
+            //设置默认文字颜色
             toBeHandle.setTextColor(Color.rgb(90, 90, 90));
             allPurchase.setTextColor(Color.rgb(90, 90, 90));
             pass.setTextColor(Color.rgb(90, 90, 90));
             refuse.setTextColor(Color.rgb(90, 90, 90));
 
+            //设置默认透明度
             toBeHandle.setAlpha(0.5f);
             allPurchase.setAlpha(0.5f);
             pass.setAlpha(0.5f);
             refuse.setAlpha(0.5f);
 
+            //设置默认边框颜色
             toBeHandle.setBackgroundResource(R.drawable.bg_border_gray);
             allPurchase.setBackgroundResource(R.drawable.bg_border_gray);
             pass.setBackgroundResource(R.drawable.bg_border_gray);
             refuse.setBackgroundResource(R.drawable.bg_border_gray);
         }
-
     }
 
     private void getPopupWindowView() {
@@ -282,8 +263,7 @@ import demo.yc.formalmanagersystem.view.RefreshableView;
         getPopupWindowView();
         resetStatusStyle();
         //默认选中全部资产与全部分类
-        if(this.isAdded())
-        {
+        if (this.isAdded()) {
             allPurchase.setAlpha(1);
             allPurchase.setTextColor(Color.rgb(95, 187, 176));
             allPurchase.setBackgroundResource(R.drawable.bg_border_green);
@@ -295,6 +275,7 @@ import demo.yc.formalmanagersystem.view.RefreshableView;
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
+            //弹出侧滑菜单按钮
             case R.id.top_layout_menu:
                 if (popupwindow != null) {
                     down = false;
@@ -302,8 +283,9 @@ import demo.yc.formalmanagersystem.view.RefreshableView;
                     popupwindow.dismiss();
                     listView.setAlpha(1);
                 }
-                ((MainActivity)getActivity()).showMenu();
+                ((MainActivity) getActivity()).showMenu();
                 break;
+            //弹出选择框按钮
             case R.id.direction_in_top:
                 if (down == false) {
                     directionImg.setImageResource(R.drawable.up);
@@ -322,6 +304,7 @@ import demo.yc.formalmanagersystem.view.RefreshableView;
                     }
                 }
                 break;
+            //返回顶部
             case R.id.back_to_top:
                 listView.smoothScrollByOffset(-getScrollY());
                 backToTop.setVisibility(View.INVISIBLE);
@@ -329,7 +312,6 @@ import demo.yc.formalmanagersystem.view.RefreshableView;
 
             //全部采购
             case R.id.all_purchase_in_drop_down_for_all_purchase:
-                temp1.clear();
                 temp2.clear();
                 for (Purchase purchase : purchases
                         ) {
@@ -345,7 +327,6 @@ import demo.yc.formalmanagersystem.view.RefreshableView;
                 break;
             //通过审核
             case R.id.pass_in_drop_down_for_all_purchase:
-                temp1.clear();
                 temp2.clear();
                 for (Purchase purchase : purchases) {
                     if (purchase.getCheckState().equals("通过")) {
@@ -362,9 +343,7 @@ import demo.yc.formalmanagersystem.view.RefreshableView;
                 break;
             //拒绝申请
             case R.id.refuse_in_drop_down_for_all_purchase:
-                temp1.clear();
                 temp2.clear();
-                temp1.clear();
                 for (Purchase purchase : purchases
                         ) {
                     if (purchase.getCheckState().equals("拒绝")) {
@@ -382,7 +361,6 @@ import demo.yc.formalmanagersystem.view.RefreshableView;
                 break;
 
             case R.id.to_be_review_in_drop_down_for_all_purchase:
-                temp1.clear();
                 temp2.clear();
                 for (Purchase purchase : purchases
                         ) {
@@ -405,7 +383,6 @@ import demo.yc.formalmanagersystem.view.RefreshableView;
     public void refreshData() {
         if (getActivity() != null) {
             //访问服务器数据
-
             volleyUtil.updateSQLiteFromMySql("purchase", new UpdateListener() {
                 @Override
                 public void onSucceed(String s) {
@@ -418,7 +395,7 @@ import demo.yc.formalmanagersystem.view.RefreshableView;
                         //解析Json格式
                         List<Purchase> lists = JsonUtil.parsePurchaseJson(s);
                         //更新本地数据库
-                        MyDBHandler.getInstance(getActivity()).updatePurchase(getActivity(),lists);
+                        MyDBHandler.getInstance(getActivity()).updatePurchase(getActivity(), lists);
                         //从本地数据库获取数据，更新界面
                         readDataFromSQLite();
                     }
@@ -427,7 +404,6 @@ import demo.yc.formalmanagersystem.view.RefreshableView;
                 @Override
                 public void onError(VolleyError error) {
                     refreshableView.finishRefreshing("all_purchase");
-                    Log.d("myTag", error.toString());
                     Message msg = new Message();
                     msg.what = 1;
                     handler.sendMessage(msg);
@@ -460,16 +436,16 @@ import demo.yc.formalmanagersystem.view.RefreshableView;
                             purchase.setPurchaseState(cursor.getString(cursor.getColumnIndex("purchaseState")));
                             purchase.setFinishTime(cursor.getString(cursor.getColumnIndex("finishTime")));
 
-                            try{
+                            try {
                                 String createrIdentifier = cursor.getString(cursor.getColumnIndex("createrIdentifier"));
                                 userCursor = db.query("User", null, "count=?", new String[]{createrIdentifier}, null, null, null, null);
                                 userCursor.moveToFirst();
                                 if (userCursor.moveToFirst()) {
                                     purchase.setCreaterName(userCursor.getString(userCursor.getColumnIndex("name")));
                                 }
-                            }catch (Exception e){
+                            } catch (Exception e) {
                                 purchase.setCreaterIdentifier("未知");
-                            }finally {
+                            } finally {
                                 userCursor.close();
                             }
                             purchases.add(purchase);
@@ -482,7 +458,6 @@ import demo.yc.formalmanagersystem.view.RefreshableView;
                 msg.obj = purchases;
                 msg.what = 0;       //处理成功
                 handler.sendMessage(msg);
-
             }
         };
         executor.execute(task1);
